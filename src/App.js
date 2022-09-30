1f32c5eb357719a2c27a7b9c6d5e6338ffa1705b
rokster112 <rokster112@gmail.com>
2022-09-30 14:21:32 +0100

    deleting a folder within a folder

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { useEffect } from 'react'
+import axios from 'axios'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+import PageNavbar from './components/PageNavbar'
+import Experience from './components/Experience'
+
+
+const App = () => {
+
+
+  return <h1>Whole page</h1>
+}
+
+export default App
