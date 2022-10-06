c0747b1b0db3c283e3e80d48934450ed5de0ce4f
rokster112 <rokster112@gmail.com>
2022-10-06 19:50:37 +0100

    second deployment

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
-import image from '../styles/images/About-image.jpeg'
+import image from '../styles/images/rokas.jpg'
 
 
 const Header = () => {
 
   return (
     <div className='Header-page' id='Header'>
-      <img className='Header-image' style={{ maxWidth: 250, maxHeight: 250, borderRadius: '50%' }} src={image}></img>
+      <img className='Header-image' style={{ maxWidth: 250, maxHeight: 250, borderRadius: 10 }} src={image}></img>
       <h1 className='Header-title'>Hello, my name is Rokas</h1>
       <p style={{ maxWidth: 900 }} className='Header-content'>I am a Lithuanian Software Engineer, who is living in London. I am currently looking for my first role as a Software Engineer, having finished General Assembly's bootcamp and gaining core Software Engineering skills.</p>
     </div>
